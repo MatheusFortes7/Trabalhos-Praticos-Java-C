d7466e1c4556829eaf7ea27c2d38b0e9cfcb7392
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2021-11-29 21:11:03 -0300

    add questao 3 tp1

--- TP01/TP01Q03 - Ciframento em Java/TP01Q03.java
+++ TP01/TP01Q03 - Ciframento em Java/TP01Q03.java
@@ -1,5 +1,5 @@
 class TP01Q03 {
-   public static boolean isFim(String s){
+   public static boolean isFim(String s){ //função para parar de ler a entrada
       return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
    }
 
