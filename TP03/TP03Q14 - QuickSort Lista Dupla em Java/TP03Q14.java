be981cbf8912e89f515648b629242e4ed4b80547
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2021-12-08 21:38:08 -0300

    add questao 14 tp3

---
+++ TP03/TP03Q14 - QuickSort Lista Dupla em Java/TP03Q14.java
@@ -0,0 +1,605 @@
+import java.io.*;
+import java.io.FileReader;
+import java.util.Date;
+import java.io.RandomAccessFile;
+
+class Serie {
+    // declaração dos atributos
+    private String name;
+    private String format;
+    private String duration;
+    private String country;
+    private String language;
+    private String broadcaster;
+    private String streaming;
+    private int seasons;
+    private int episodes;
+
+    // construtor primário
+    public Serie() {
+        name = "";
+        format = "";
+        duration = "";
+        country = "";
+        language = "";
+        broadcaster = "";
+        streaming = "";
+        seasons = 0;
+        episodes = 0;
+    }
+
+    // construtor secundário
+    public Serie(String name, String format, String duration, String country, String language, String broadcaster,
+            String streaming, int seasons, int episodes) {
+        this.name = name;
+        this.format = format;
+        this.duration = duration;
+        this.country = country;
+        this.language = language;
+        this.broadcaster = broadcaster;
+        this.streaming = streaming;
+        this.seasons = seasons;
+        this.episodes = episodes;
+    }
+
+    // método para setar o atributo name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // método para setar o atributo formato
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    // método para setar o atributo duration
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    // método para setar o atributo country
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    // método para setar o atributo language
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    // método para setar o atributo broadcaster
+    public void setBroadcaster(String broadcaster) {
+        this.broadcaster = broadcaster;
+    }
+
+    // método para setar o atributo streaming
+    public void setStreaming(String streaming) {
+        this.streaming = streaming;
+    }
+
+    // método para setar o atributo seasons
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    // método para setar o atributo episodes
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    // método para retornar o atributo name
+    public String getName() {
+        return this.name;
+    }
+
+    // método para retornar o atributo format
+    public String getFormat() {
+        return this.format;
+    }
+
+    // método para retornar o atributo duration
+    public String getDuration() {
+        return this.duration;
+    }
+
+    // método para retornar o atributo country
+    public String getCountry() {
+        return this.country;
+    }
+
+    // método para retornar o atributo language
+    public String getLanguage() {
+        return this.language;
+    }
+
+    // método para retornar o atributo broadcaster
+    public String getBroadcaster() {
+        return this.broadcaster;
+    }
+
+    // método para retornar o atributo streaming
+    public String getStreaming() {
+        return this.streaming;
+    }
+
+    // método para retornar o atributo seasons
+    public int getSeasons() {
+        return this.seasons;
+    }
+
+    // método para retornar o atributo episodes
+    public int getEpisodes() {
+        return this.episodes;
+    }
+
+    // método para clonar a classe
+    public Serie clone() {
+        Serie resp = new Serie();
+        resp.name = this.name;
+        resp.format = this.format;
+        resp.duration = this.duration;
+        resp.country = this.country;
+        resp.language = this.language;
+        resp.broadcaster = this.broadcaster;
+        resp.streaming = this.streaming;
+        resp.seasons = this.seasons;
+        resp.episodes = this.episodes;
+        return resp;
+    }
+
+    // método para printar a classe
+    public void printClass() {
+        System.out
+                .println(this.name + " " + this.format + " " + this.duration + " " + this.country + " " + this.language
+                        + " " + this.broadcaster + " " + this.streaming + " " + this.seasons + " " + this.episodes);
+    }
+
+    // método para tratar a linha, deixar apenas números e converter o retorno de
+    // String para Integer
+    public int justInt(String line) {
+        String resp = "";
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) >= '0' && line.charAt(i) <= '9') { // caso o caracter seja um número ele é concatenado a
+                                                                  // variável resp
+                resp += line.charAt(i);
+            } else { // caso seja outro caracter, o i recebe o valor da condição de parada e o método
+                     // de repetição é encerrado
+                i = line.length();
+            }
+        }
+        return Integer.parseInt(resp); // conversão da string resp para número inteiro a ser retornado
+    }
+
+    // método para a remoção das tags da linha lida do arquivo para retornar apenas
+    // o que é desejado
+    public String removeTags(String line) {
+        String resp = "";
+        int i = 0;
+        while (i < line.length()) { // enquanto i for menor que o tamanho da String linha
+            if (line.charAt(i) == '<') { // é testado para verificar se o contador i ainda está dentro das tags
+                i++;
+                while (line.charAt(i) != '>')
+                    i++; // ao encontrar o sinal de fechamento das tags o laço de repetição é encerrado
+            } else if (line.charAt(i) == '&') { // mesmo tratamento de cima mas para outras exceções presentes em alguns
+                                                // outros arquivos
+                i++;
+                while (line.charAt(i) != ';')
+                    i++;
+            } else { // o que estiver fora das tags é concatenado a String resp a ser retornada
+                resp += line.charAt(i);
+            }
+            i++;
+        }
+        // System.out.println(resp);
+        return resp;
+    }
+
+    // método para tratar o nome do arquivo e retornar o nome da série
+    public String searchName(String fileName) {
+        String resp = "";
+        for (int i = 0; i < fileName.length(); i++) {
+            if (fileName.charAt(i) == '_') { // caso o caracter na posição i seja igual ao '_' a variável resp recebe um
+                                             // espaço em branco
+                resp += ' ';
+            } else { // caso não tenha espaço em branco o caracter é concatenado à string resp
+                resp += fileName.charAt(i);
+            }
+        }
+        return resp.substring(0, resp.length() - 5); // retorno da substring resp retirando os 5 últimos caracteres
+                                                     // relacionados à extensão do arquivo
+    }
+
+    // método para leitura do arquivo .html e tratamento das linhas
+    public void readClass(String fileName) throws Exception {
+        String file = "/tmp/series/" + fileName;
+        try {
+            FileReader fileReader = new FileReader(file); // declaração da variável fileReader que será recebida pelo
+                                                          // bufferedReader
+
+            BufferedReader br = new BufferedReader(fileReader); // declaração do bufferedReader para leitura do arquivo
+
+            // set nome da série
+            this.name = searchName(fileName);
+
+            // set Formato da série
+            while (!br.readLine().contains("Formato"))
+                ;
+            this.format = removeTags(br.readLine()).trim();
+
+            // set duração da série
+            while (!br.readLine().contains("Duração"))
+                ;
+            this.duration = removeTags(br.readLine()).trim();
+
+            // set país da série
+            while (!br.readLine().contains("País de origem"))
+                ;
+            this.country = removeTags(br.readLine()).trim();
+
+            // set idioma da série
+            while (!br.readLine().contains("Idioma original"))
+                ;
+            this.language = removeTags(br.readLine()).trim();
+
+            // set emissora da série
+            while (!br.readLine().contains("Emissora de televisão"))
+                ;
+            this.broadcaster = removeTags(br.readLine()).trim();
+
+            // set transmissão original da série
+            while (!br.readLine().contains("Transmissão original"))
+                ;
+            this.streaming = removeTags(br.readLine()).trim();
+
+            // set temporadas da série
+            while (!br.readLine().contains("N.º de temporadas"))
+                ;
+            this.seasons = justInt(removeTags(br.readLine()));
+
+            // set episódios da série
+            while (!br.readLine().contains("N.º de episódios"))
+                ;
+            this.episodes = justInt(removeTags(br.readLine()));
+
+            // fechamento do bufferedReader
+            br.close();
+            // Tratamento de exceções
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to open file '" + fileName + "'");
+        } catch (IOException e) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+
+    }
+}
+
+class CelulaDupla {
+    public Serie elemento;
+    public CelulaDupla ant;
+    public CelulaDupla prox;
+
+    /**
+     * Construtor da classe.
+     */
+    public CelulaDupla() {
+        this(null);
+    }
+
+    /**
+     * Construtor da classe.
+     * 
+     * @param elemento int inserido na celula.
+     */
+    public CelulaDupla(Serie elemento) {
+        this.elemento = elemento;
+        this.ant = this.prox = null;
+    }
+}
+
+class ListaDupla {
+    private CelulaDupla primeiro;
+    private CelulaDupla ultimo;
+
+    /**
+     * Construtor da classe que cria uma lista dupla sem elementos (somente no
+     * cabeca).
+     */
+    public ListaDupla() {
+        primeiro = new CelulaDupla();
+        ultimo = primeiro;
+    }
+
+    /**
+     * Insere um elemento na primeira posicao da lista.
+     * 
+     * @param x int elemento a ser inserido.
+     */
+    public void inserirInicio(Serie x) {
+        CelulaDupla tmp = new CelulaDupla(x);
+
+        tmp.ant = primeiro;
+        tmp.prox = primeiro.prox;
+        primeiro.prox = tmp;
+        if (primeiro == ultimo) {
+            ultimo = tmp;
+        } else {
+            tmp.prox.ant = tmp;
+        }
+        tmp = null;
+    }
+
+    /**
+     * Insere um elemento na ultima posicao da lista.
+     * 
+     * @param x int elemento a ser inserido.
+     */
+    public void inserirFim(Serie x) {
+        ultimo.prox = new CelulaDupla(x);
+        ultimo.prox.ant = ultimo;
+        ultimo = ultimo.prox;
+    }
+
+    /**
+     * Remove um elemento da primeira posicao da lista.
+     * 
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se a lista nao contiver elementos.
+     */
+    public Serie removerInicio() throws Exception {
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+        }
+
+        CelulaDupla tmp = primeiro;
+        primeiro = primeiro.prox;
+        Serie resp = primeiro.elemento;
+        tmp.prox = primeiro.ant = null;
+        tmp = null;
+        return resp;
+    }
+
+    /**
+     * Remove um elemento da ultima posicao da lista.
+     * 
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se a lista nao contiver elementos.
+     */
+    public Serie removerFim() throws Exception {
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+        }
+        Serie resp = ultimo.elemento;
+        ultimo = ultimo.ant;
+        ultimo.prox.ant = null;
+        ultimo.prox = null;
+        return resp;
+    }
+
+    /**
+     * Insere um elemento em uma posicao especifica considerando que o primeiro
+     * elemento valido esta na posicao 0.
+     * 
+     * @param x   int elemento a ser inserido.
+     * @param pos int posicao da insercao.
+     * @throws Exception Se <code>posicao</code> invalida.
+     */
+    public void inserir(Serie x, int pos) throws Exception {
+
+        int tamanho = tamanho();
+
+        if (pos < 0 || pos > tamanho) {
+            throw new Exception("Erro ao inserir posicao (" + pos + " / tamanho = " + tamanho + ") invalida!");
+        } else if (pos == 0) {
+            inserirInicio(x);
+        } else if (pos == tamanho) {
+            inserirFim(x);
+        } else {
+            // Caminhar ate a posicao anterior a insercao
+            CelulaDupla i = primeiro;
+            for (int j = 0; j < pos; j++, i = i.prox)
+                ;
+
+            CelulaDupla tmp = new CelulaDupla(x);
+            tmp.ant = i;
+            tmp.prox = i.prox;
+            tmp.ant.prox = tmp.prox.ant = tmp;
+            tmp = i = null;
+        }
+    }
+
+    /**
+     * Remove um elemento de uma posicao especifica da lista considerando que o
+     * primeiro elemento valido esta na posicao 0.
+     * 
+     * @param posicao Meio da remocao.
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se <code>posicao</code> invalida.
+     */
+    public Serie remover(int pos) throws Exception {
+        Serie resp;
+        int tamanho = tamanho();
+
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+
+        } else if (pos < 0 || pos >= tamanho) {
+            throw new Exception("Erro ao remover (posicao " + pos + " / " + tamanho + " invalida!");
+        } else if (pos == 0) {
+            resp = removerInicio();
+        } else if (pos == tamanho - 1) {
+            resp = removerFim();
+        } else {
+            // Caminhar ate a posicao anterior a insercao
+            CelulaDupla i = primeiro.prox;
+            for (int j = 0; j < pos; j++, i = i.prox)
+                ;
+
+            i.ant.prox = i.prox;
+            i.prox.ant = i.ant;
+            resp = i.elemento;
+            i.prox = i.ant = null;
+            i = null;
+        }
+
+        return resp;
+    }
+
+    /**
+     * Mostra os elementos da lista separados por espacos.
+     */
+    public void mostrar() {
+       
+        for (CelulaDupla i = primeiro.prox; i != null; i = i.prox) {
+            i.elemento.printClass();
+        }
+
+    }
+
+    /**
+     * Mostra os elementos da lista de forma invertida e separados por espacos.
+     */
+    public void mostrarInverso() {
+        System.out.print("[ ");
+        for (CelulaDupla i = ultimo; i != primeiro; i = i.ant) {
+            System.out.print(i.elemento + " ");
+        }
+        System.out.println("] "); // Termina de mostrar.
+    }
+
+    /**
+     * Procura um elemento e retorna se ele existe.
+     * 
+     * @param x Elemento a pesquisar.
+     * @return <code>true</code> se o elemento existir, <code>false</code> em caso
+     *         contrario.
+     */
+    public boolean pesquisar(Serie x) {
+        boolean resp = false;
+        for (CelulaDupla i = primeiro.prox; i != null; i = i.prox) {
+            if (i.elemento == x) {
+                resp = true;
+                i = ultimo;
+            }
+        }
+        return resp;
+    }
+
+    /**
+     * Calcula e retorna o tamanho, em numero de elementos, da lista.
+     * 
+     * @return resp int tamanho
+     */
+    public int tamanho() {
+        int tamanho = 0;
+        for (CelulaDupla i = primeiro; i != ultimo; i = i.prox, tamanho++);
+        return tamanho;
+    }
+
+    public void quickSort() {
+        quicksort(0, tamanho()-1);
+    }
+
+    public CelulaDupla getCelulaIndex(int pos){
+        int tmp = 0;
+        CelulaDupla i;
+        for( i = primeiro.prox; tmp < pos ; i = i.prox, tmp++);
+        return i;
+    }
+
+    private void quicksort(int esq, int dir) {
+        int i = esq, j = dir;
+        Serie pivo = getCelulaIndex((esq + dir)/2 ).elemento;
+        while (i <= j) {
+            CelulaDupla aux = getCelulaIndex(i);
+            TP03Q14.contador++;
+            while ((aux.elemento.getCountry().compareTo(pivo.getCountry()) < 0) || ((aux.elemento.getCountry().compareTo(pivo.getCountry()) == 0) && (aux.elemento.getName().compareTo(pivo.getName()) < 0))){
+                i++;
+                aux = aux.prox;
+            }
+            CelulaDupla index = getCelulaIndex(j);
+            while ((index.elemento.getCountry().compareTo(pivo.getCountry()) > 0) || ((index.elemento.getCountry().compareTo(pivo.getCountry()) == 0) && (index.elemento.getName().compareTo(pivo.getName()) > 0))){
+                j--;
+                index = index.ant;
+            }
+
+            if (i <= j) {
+                TP03Q14.contador++;
+                swap(i, j);
+                i++;
+                j--;
+            }
+        }
+        if (esq < j) {
+            TP03Q14.contador++;
+            quicksort(esq, j);
+        }
+        if (i < dir) {
+            TP03Q14.contador++;
+            quicksort(i, dir);
+        }
+
+    }
+
+    public void swap(int i, int primeiro) {
+        Serie aux = getCelulaIndex(i).elemento;
+        getCelulaIndex(i).elemento = getCelulaIndex(primeiro).elemento;
+        getCelulaIndex(primeiro).elemento = aux;
+    }
+
+}
+
+class TP03Q14 {
+    public static int contador = 0;
+    // Salvando os itens no arra nao dara certo pois so ordenara os paises, e nao a
+    // linha inteira
+
+    public static long now() {
+        return new Date().getTime();
+    }
+
+    public static boolean isFim(String s) {
+        return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    public static void main(String[] args) throws Exception {
+        Serie serie = new Serie();
+        String[] input = new String[1000];
+        ListaDupla lista = new ListaDupla();
+        int numInput = 0;
+        long inicio = 0, fim = 0;
+        double diferenca = 0;
+
+        inicio = now();
+
+        do {
+            input[numInput] = MyIO.readLine();
+        } while (isFim(input[numInput++]) == false);
+        numInput--;// Desconsiderar a palavra FIM
+
+        for (int i = 0; i < numInput; i++) {
+            try {
+                TP03Q14.contador++;
+                serie = new Serie();
+                serie.readClass(input[i]);
+                // serie.printClass();
+                lista.inserirFim(serie);
+            } catch (Exception e) {
+            }
+        }
+        // lista.mostrar();
+        // System.out.println("----------------");
+        // System.out.println("ordenado");
+        // System.out.println("----------------");
+        lista.quickSort();
+        lista.mostrar();
+
+        fim = now();
+        diferenca = (fim - inicio) / 1000.0;
+
+        RandomAccessFile Arq = new RandomAccessFile("747358_quicksort2.txt", "rw");
+
+        Arq.writeChars("747358" + "\t" + diferenca + "\t" + TP03Q14.contador);
+
+        Arq.close();
+    }
+}
